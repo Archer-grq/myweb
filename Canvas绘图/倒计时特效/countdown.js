832df12472940968d6e917e7bee36518d252dbe3
HASEE <18300466806@163.com>
2018-12-03 16:40:27 +0800

    Delete jj.txt

--- Canvas绘图/倒计时特效/countdown.js
+++ Canvas绘图/倒计时特效/countdown.js
@@ -1,152 +1,180 @@
-var WINDOW_WIDTH=1024;
-var WINDOW_HEIGHT=768;
-var RADIUS=8;
-var MARGIN_TOP=60;
-var MARGIN_LEFT=30;
+var WINDOW_WIDTH = 1024;
+var WINDOW_HEIGHT = 768;
+var RADIUS = 8;
+var MARGIN_TOP = 60;
+var MARGIN_LEFT = 30;
 
-//const endTime=new Date(2018,11,5,11,11,11);//倒计时停止时间
-var curShowTimeSeconds = 0;//存储倒计时的秒数
+//var endTime = new Date();
+//endTime.setTime( endTime.getTime() + 3600*1000 )
+var curShowTimeSeconds = 0
 
-var balls=[];
-const colors=["#33b5e5","#0099cc","#aa66cc","#9933cc","#99cc00","#669900","#ffbb33","#ff8800","#ff4444","#cc0000"];
+var balls = [];
+const colors = ["#33B5E5","#0099CC","#AA66CC","#9933CC","#99CC00","#669900","#FFBB33","#FF8800","#FF4444","#CC0000"]
 
+window.onload = function(){
 
-window.onload=function(){
+    WINDOW_WIDTH = document.body.clientWidth
+    WINDOW_HEIGHT = document.body.clientHeight
 
-    WINDOW_WIDTH=document.body.clientWidth;
-    WINDOW_HEIGHT=document.body.clientHeight;
-    MARGIN_LEFT=Math.round(WINDOW_WIDTH/10);
-    RADIUS=Math.round(WINDOW_WIDTH*4/5/108)-1;
-    MARGIN_TOP=Math.round(WINDOW_HEIGHT/5);
+    MARGIN_LEFT = Math.round(WINDOW_WIDTH /10);
+    RADIUS = Math.round(WINDOW_WIDTH * 4 / 5 / 108)-1
 
-    var canvas = document.getElementById("canvas");//获取画布
+    MARGIN_TOP = Math.round(WINDOW_HEIGHT /5);
+
+    var canvas = document.getElementById('canvas');
     var context = canvas.getContext("2d");
-    canvas.width=WINDOW_WIDTH;//设置画布宽度
-    canvas.height=WINDOW_HEIGHT;//设置画布高度
 
-    curShowTimeSeconds=getCurrentShowTimeSeconds();//通过函数获取倒计时的秒数
+    canvas.width = WINDOW_WIDTH;
+    canvas.height = WINDOW_HEIGHT;
 
-    setInterval(function(){
-        render(context);
-        update();
-    },50)
-}
-
-function update(){
-    var nextShowTimeSeconds=getCurrentShowTimeSeconds();
-
-    var nextHours=parseInt(nextShowTimeSeconds/3600);
-    var nextMinutes=parseInt((nextShowTimeSeconds-nextHours*3600)/60);
-    var nextSeconds=parseInt(nextShowTimeSeconds%60);
-
-    var curHours=parseInt(curShowTimeSeconds/3600);
-    var curMinutes=parseInt((curShowTimeSeconds-curHours*3600)/60);
-    var curSeconds=parseInt(curShowTimeSeconds%60);
-
-    if(nextSeconds!=curSeconds){
-        if(parseInt(curHours/10)!=parseInt(nextHours/10))
-            addBalls(MARGIN_LEFT+0,MARGIN_TOP,parseInt(curHours/10));
-        if(parseInt(curHours%10)!=parseInt(nextHours%10))
-            addBalls(MARGIN_LEFT+15*(RADIUS+1),MARGIN_TOP,parseInt(curHours%10));
-
-        if(parseInt(curMinutes/10)!=parseInt(nextMinutes/10))
-            addBalls(MARGIN_LEFT+39*(RADIUS+1),MARGIN_TOP,parseInt(curMinutes/10));
-        if(parseInt(curMinutes%10)!=parseInt(nextMinutes%10))
-            addBalls(MARGIN_LEFT+54*(RADIUS+1),MARGIN_TOP,parseInt(curMinutes%10));
-
-        if(parseInt(curSeconds/10)!=parseInt(nextSeconds/10))
-            addBalls(MARGIN_LEFT+78*(RADIUS+1),MARGIN_TOP,parseInt(curSeconds/10));
-        if(parseInt(curSeconds%10)!=parseInt(nextSeconds%10))
-            addBalls(MARGIN_LEFT+93*(RADIUS+1),MARGIN_TOP,parseInt(curSeconds%10));
-
-        curShowTimeSeconds=nextShowTimeSeconds;
-    }
-    updateBalls();
-}
-
-function updateBalls(){
-    for(var i=0;i<balls.length;i++){
-        balls[i].x+=balls[i].vx;
-        balls[i].y+=balls[i].vy;
-        balls[i].vy+=balls[i].g;
-
-        if(balls[i].y>=WINDOW_HEIGHT-RADIUS){
-            balls[i].y=WINDOW_HEIGHT-RADIUS;
-            balls[i].vy=-(balls[i].vy*0.75);
+    curShowTimeSeconds = getCurrentShowTimeSeconds()
+    setInterval(
+        function(){
+            render( context );
+            update();
         }
-    }
-    var cnt=0;
-    for(var i=0;i<balls.length;i++)
-        if(balls[i].x+RADIUS>0&&balls[i].x-RADIUS<WINDOW_WIDTH)
-            balls[cnt++]=balls[i]
-    while(balls.length>cnt)
-    balls.pop();
+        ,
+        50
+    );
 }
 
-//添加小球
-function addBalls(x,y,num) {
-    for(var i=0;i<digit[num].length;i++)
-        for(var j=0;j<digit[num][i].length;j++){
-            if(digit[num][i][j]==0){
-                var aBall={
-                    x:x+i*(RADIUS+1)+(RADIUS+1),
-                    y:y+j*(RADIUS+1)+(RADIUS+1),
-                    g:1.5+Math.random(),
-                    vx:Math.pow(-1,Math.ceil(Math.random()*1000))*4,
-                    vy:-5,
-                    color:colors[Math.floor(Math.random()*colors.length)]
-                }
+function getCurrentShowTimeSeconds() {
+    var curTime = new Date();
+    var ret = curTime.getHours() * 3600 + curTime.getMinutes() * 60 + curTime.getSeconds();
 
-                balls.push(aBall);
-            }
-        }
-
-}
-
-//时间函数
-function getCurrentShowTimeSeconds(){
-    var curTime=new Date();
-    var ret=curTime.getHours()*3600+curTime.getMinutes()*60+curTime.getSeconds();
     return ret;
 }
 
-//操作执行函数
-function render(cxt){
+function update(){
 
-    cxt.clearRect(0,0,WINDOW_WIDTH,WINDOW_HEIGHT);
+    var nextShowTimeSeconds = getCurrentShowTimeSeconds();
 
-    var hours=parseInt(curShowTimeSeconds/3600);
-    var minutes=parseInt((curShowTimeSeconds-hours*3600)/60);
-    var seconds=parseInt(curShowTimeSeconds%60);
+    var nextHours = parseInt( nextShowTimeSeconds / 3600);
+    var nextMinutes = parseInt( (nextShowTimeSeconds - nextHours * 3600)/60 )
+    var nextSeconds = nextShowTimeSeconds % 60
 
-    renderDigit(MARGIN_LEFT, MARGIN_TOP,parseInt(hours/10),cxt);
-    renderDigit(MARGIN_LEFT+15*(RADIUS+1),MARGIN_TOP,parseInt(hours%10),cxt);
-    renderDigit(MARGIN_LEFT+30*(RADIUS+1),MARGIN_TOP,10,cxt);
-    renderDigit(MARGIN_LEFT+39*(RADIUS+1),MARGIN_TOP,parseInt(minutes/10),cxt);
-    renderDigit(MARGIN_LEFT+54*(RADIUS+1),MARGIN_TOP,parseInt(minutes%10),cxt);
-    renderDigit(MARGIN_LEFT+69*(RADIUS+1),MARGIN_TOP,10,cxt);
-    renderDigit(MARGIN_LEFT+78*(RADIUS+1),MARGIN_TOP,parseInt(seconds/10),cxt);
-    renderDigit(MARGIN_LEFT+93*(RADIUS+1),MARGIN_TOP,parseInt(seconds%10),cxt);
-    //画球
-    for(var i=0;i<balls.length;i++){
+    var curHours = parseInt( curShowTimeSeconds / 3600);
+    var curMinutes = parseInt( (curShowTimeSeconds - curHours * 3600)/60 )
+    var curSeconds = curShowTimeSeconds % 60
+
+    if( nextSeconds != curSeconds ){
+        if( parseInt(curHours/10) != parseInt(nextHours/10) ){
+            addBalls( MARGIN_LEFT + 0 , MARGIN_TOP , parseInt(curHours/10) );
+        }
+        if( parseInt(curHours%10) != parseInt(nextHours%10) ){
+            addBalls( MARGIN_LEFT + 15*(RADIUS+1) , MARGIN_TOP , parseInt(curHours/10) );
+        }
+
+        if( parseInt(curMinutes/10) != parseInt(nextMinutes/10) ){
+            addBalls( MARGIN_LEFT + 39*(RADIUS+1) , MARGIN_TOP , parseInt(curMinutes/10) );
+        }
+        if( parseInt(curMinutes%10) != parseInt(nextMinutes%10) ){
+            addBalls( MARGIN_LEFT + 54*(RADIUS+1) , MARGIN_TOP , parseInt(curMinutes%10) );
+        }
+
+        if( parseInt(curSeconds/10) != parseInt(nextSeconds/10) ){
+            addBalls( MARGIN_LEFT + 78*(RADIUS+1) , MARGIN_TOP , parseInt(curSeconds/10) );
+        }
+        if( parseInt(curSeconds%10) != parseInt(nextSeconds%10) ){
+            addBalls( MARGIN_LEFT + 93*(RADIUS+1) , MARGIN_TOP , parseInt(nextSeconds%10) );
+        }
+
+        curShowTimeSeconds = nextShowTimeSeconds;
+    }
+
+    updateBalls();
+
+    console.log( balls.length)
+}
+
+function updateBalls(){
+
+    for( var i = 0 ; i < balls.length ; i ++ ){
+
+        balls[i].x += balls[i].vx;
+
+        var c = 1.0;
+        if( balls[i].y + RADIUS + balls[i].vy >= WINDOW_HEIGHT ){
+            c = ( WINDOW_HEIGHT - (balls[i].y+ RADIUS) ) / balls[i].vy;
+            console.log( c );
+        }
+
+        balls[i].y += balls[i].vy;
+        balls[i].vy += c * balls[i].g;;
+
+        if( balls[i].y >= WINDOW_HEIGHT-RADIUS ){
+            balls[i].y = WINDOW_HEIGHT-RADIUS;
+            balls[i].vy = - Math.abs(balls[i].vy)*0.75;
+        }
+    }
+
+    var cnt = 0
+    for( var i = 0 ; i < balls.length ; i ++ )
+        if( balls[i].x + RADIUS > 0 && balls[i].x -RADIUS < WINDOW_WIDTH )
+            balls[cnt++] = balls[i]
+
+    while( balls.length > cnt ){
+        balls.pop();
+    }
+}
+
+function addBalls( x , y , num ){
+
+    for( var i = 0  ; i < digit[num].length ; i ++ )
+        for( var j = 0  ; j < digit[num][i].length ; j ++ )
+            if( digit[num][i][j] == 1 ){
+                var aBall = {
+                    x:x+j*2*(RADIUS+1)+(RADIUS+1),
+                    y:y+i*2*(RADIUS+1)+(RADIUS+1),
+                    g:1.5+Math.random(),
+                    vx:Math.pow( -1 , Math.ceil( Math.random()*1000 ) ) * 4,
+                    vy:-5,
+                    color: colors[ Math.floor( Math.random()*colors.length ) ]
+                }
+
+                balls.push( aBall )
+            }
+}
+
+function render( cxt ){
+
+    cxt.clearRect(0,0,WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    var hours = parseInt( curShowTimeSeconds / 3600);
+    var minutes = parseInt( (curShowTimeSeconds - hours * 3600)/60 )
+    var seconds = curShowTimeSeconds % 60
+
+    renderDigit( MARGIN_LEFT , MARGIN_TOP , parseInt(hours/10) , cxt )
+    renderDigit( MARGIN_LEFT + 15*(RADIUS+1) , MARGIN_TOP , parseInt(hours%10) , cxt )
+    renderDigit( MARGIN_LEFT + 30*(RADIUS + 1) , MARGIN_TOP , 10 , cxt )
+    renderDigit( MARGIN_LEFT + 39*(RADIUS+1) , MARGIN_TOP , parseInt(minutes/10) , cxt);
+    renderDigit( MARGIN_LEFT + 54*(RADIUS+1) , MARGIN_TOP , parseInt(minutes%10) , cxt);
+    renderDigit( MARGIN_LEFT + 69*(RADIUS+1) , MARGIN_TOP , 10 , cxt);
+    renderDigit( MARGIN_LEFT + 78*(RADIUS+1) , MARGIN_TOP , parseInt(seconds/10) , cxt);
+    renderDigit( MARGIN_LEFT + 93*(RADIUS+1) , MARGIN_TOP , parseInt(seconds%10) , cxt);
+
+    for( var i = 0 ; i < balls.length ; i ++ ){
         cxt.fillStyle=balls[i].color;
+
         cxt.beginPath();
-        cxt.arc(balls[i].x,balls[i].y,RADIUS,0,Math.PI*2,true);
+        cxt.arc( balls[i].x , balls[i].y , RADIUS , 0 , 2*Math.PI , true );
         cxt.closePath();
+
         cxt.fill();
     }
 }
 
-//作图函数
-function renderDigit(x,y,num,cxt){
-  for(var i=0;i<digit[num].length;i++)
-      for(var j=0; j<digit[num][i].length;j++)
-          if(digit[num][i][j]===1){
-              cxt.beginPath();
-              cxt.arc(x+j*2*(RADIUS+1)+(RADIUS+1),y+2*i*(RADIUS+1)+(RADIUS+1),RADIUS,0,2*Math.PI);
-              cxt.closePath();
-              cxt.fillStyle="green";
-              cxt.fill();
+function renderDigit( x , y , num , cxt ){
 
-          }
+    cxt.fillStyle = "rgb(0,102,153)";
+
+    for( var i = 0 ; i < digit[num].length ; i ++ )
+        for(var j = 0 ; j < digit[num][i].length ; j ++ )
+            if( digit[num][i][j] == 1 ){
+                cxt.beginPath();
+                cxt.arc( x+j*2*(RADIUS+1)+(RADIUS+1) , y+i*2*(RADIUS+1)+(RADIUS+1) , RADIUS , 0 , 2*Math.PI )
+                cxt.closePath()
+
+                cxt.fill()
+            }
 }
